2a323e3d64e47bffff0ad91b5f9d03d6daaa4bd3
Mike McRill <mike@mcrill.net>
2024-01-03 21:38:54 -0600

    Add release notes to releases

--- shell/rot.sh
+++ shell/rot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 export APP_NAME=rot
+export APP_URL=https://rotx.dev
 export GITHUB_REPOSITORY_ID=723736527
 export INSTALL_ALL="install-go install-golangci-lint install-hugo install-shellcheck install-vault"
